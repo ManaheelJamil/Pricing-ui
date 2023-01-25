bd85d1ae84f6ef979cda427e56dfc33d0b1d8847
Manaheel jamil <96910538+ManaheelJamil@users.noreply.github.com>
2023-01-25 11:19:13 +0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 "use client"
 import { Flex, Spacer, Box, Heading, Text } from '@chakra-ui/react'
-import { Grid, GridItem, SimpleGrid, Button } from '@chakra-ui/react'
+import { Grid, GridItem, SimpleGrid, Stack } from '@chakra-ui/react'
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 import {
   List,
   ListItem,
@@ -8,14 +10,13 @@ import {
   OrderedList,
   UnorderedList,
 } from '@chakra-ui/react'
-import { Icon, createIcon } from '@chakra-ui/react'
-// import { Icon } from '@chakra-ui/react'
-import { CheckCircleIcon } from 'react-icons/md'
+import { Icon } from '@chakra-ui/react'
+
 const Home = () => {
   return (<>
-    <SimpleGrid columns={2} spacing={0} >
-      <Box bg='teal' height='15vh' w="150vh" p="10" textAlign="center" fontSize="50px" fontFamily="sans-serif" color="white">
-        Simple pricing For Your Business
+    {/* <SimpleGrid columns={2} spacing={1} >
+      <Box bg='teal' height='20vh' w="150vh" p="10" textAlign="center" fontSize="50px" fontFamily="sans-serif" color="white">
+        Simple pricing For Your business
       </Box>
     </SimpleGrid>
     <SimpleGrid >
@@ -38,13 +39,13 @@ const Home = () => {
         </Box> </GridItem>
   <GridItem colSpan={0} bg='white' w="75vh" h="40vh">
     <Box m="8" fontSize="20px" >Access this feature when you get this pricing packages for your business</Box>
-  <UnorderedList  left="10px"spacing={2} fontSize="18px" listStyleType="none" >
+  <UnorderedList  left="10px"spacing={2} fontSize="16px" listStyleType="none" >
   <ListItem><Icon viewBox='0 0 200 200' color='teal'>
   <path
     fill='currentColor'
-    d1='M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0'
+    d='M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0'
   />
-</Icon>    International calling and messaging API</ListItem>
+</Icon>   International calling and messaging API</ListItem>
   <ListItem><Icon viewBox='0 0 200 200' color='teal'>
   <path
     fill='currentColor'
@@ -66,216 +67,84 @@ const Home = () => {
 </UnorderedList>
   </GridItem>
 </Grid>
-    <SimpleGrid bg="RGBA(0, 0, 0, 0.09)" height='40vh' w="150vh"></SimpleGrid>
-    {/* <Grid> 
-  <GridItem 
-position="absolute"
-width= "1440px"
-height=" 397px"
-left= "0px"
-top="0px"
-background= "#6B46C1">
-  <Heading 
-  position= "absolute"
-width= "751px"
-height="62.31px"
-left="344px"
-top= "88.45px"
-fontFamily= 'Initial'
-fontStyle="normal"
-fontWeigh="800"
-fontSize= "50px"
-lineHeight= "130%"
-textAlign="center"
-letterSpacing="-0.01em"
-color="#F7FAFC">
-    Simple pricing for your business
-  </Heading>
-  <Heading as='h5' 
-  position="absolute"
-width="604px"
-height=" 32.16px"
-left= "418px"
-top="166.84px"
-fontFamily='Initial'
-fontStyle="normal"
-fontWeight="500"
-fontSize="24px"
-lineHeight="32px"
-textAlign="center"
-color ="#F7FAFC"
-opacity= "0.84">
- Plans that are carefully crafted to suit your business
-  </Heading>
-  </GridItem >
- <Grid
-color="pink"
-  // gap={1}
- top="220px"
-  left="90px"
-   z-index="-1"
-    position="absolute" >
-  <GridItem 
-  position= "absolute"
-width= "994px"
-height= "336px"
-left= "220px"
-// top= "256px"
-background=" #FFFFFF"
-shadow= "0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04)"
-border-radius= "12px" > 
-  <Box position=" absolute"
-  z-index="-1"
-width= "378px"
-height= "336px"
-left= "220px"
-top="257px"
-
-background="#652CD3"
->Premium Pro
-          <Heading 
-          position= "absolute"
-width= "157px"
-height= "60px"
-left="329px"
-top="362px"
-fontFamily='Inter'
-fontStyle=" normal"
-fontWeight="800"
-fontSize="60px"
-lineHeight="100%"
-letter-spacing= "-0.01em"
-color= "#171923">$329</Heading>
-          <Heading position= "absolute"
-width= "282px"
-height= "28px"
-left= "266px"
-top= "430px"
-fontFamily= 'Inter'
-fontStyle="normal"
-font-weight="500"
-fontSize= "18px"
-lineHeight= "28px"
-text-align="center"
-color="#171923">Billed just Once</Heading> <br />
-          <Button 
-position="absolute"
-width="282px"
-height= "51px"
-left= "268px"
-top=" 482px"
-background="#805AD5"
-borderRadius= "8px">Get Started</Button>
-        </Box> </GridItem>
-  <GridItem colSpan={0} bg='white' w="75vh" h="40vh">
-    <Box position=" absolute"
-width= "520px"
-height= "56px"
-left= "646px"
-top="304px"
-fontFamily='Inter'
-fontStyle="normal"
-fontWeight=" 400"
-fontSize="18px"
-lineHeight=" 28px"
-color= "#2D3748"
- >Access this feature when you get this pricing packages for your business</Box>
-  <UnorderedList  left="10px"spacing={2} fontSize="18px" listStyleType="none" >
-  <ListItem
-   position="absolute"
-width="490px"
-height= "28px"
-left="688px"
-top="384px"
-fontFamily='Inter'
-fontStyle= "normal"
-fontWeight= "400"
-fontSize="18px"
-lineHeight=" 28px"
-color=" #2D3748"
-><Icon viewBox='0 0 200 200' color='teal'>
-  <path
-    fill='currentColor'
-    d1='M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0'
-    position="absolute"
-left="45.21%"
-right= "53.96%"
-top=" 47.36%"
-bottom="51.56%"
-background=" #FFFFFF"
-  />
-</Icon>    International calling and messaging API</ListItem>
-  <ListItem  position="absolute"
-width="490px"
-height= "28px"
-left="688px"
-top="428px"
-fontFamily='Inter'
-fontStyle= "normal"
-fontWeight= "400"
-fontSize="18px"
-lineHeight=" 28px"
-color=" #2D3748"><Icon viewBox='0 0 200 200' color='teal'>
+    <SimpleGrid bg="teal" height='40vh' w="150vh"></SimpleGrid> */}
+    <Grid m="5"bg="blackAlpha.100" w="80vh" shadow="5px 10px 10px" 
+    borderRadius="20px" h="60vh"><Text
+      bg="blackAlpha.600"
+      bgClip='text'
+      fontSize='8xl'
+      fontWeight='extrabold'
+      fontFamily="Serif"  m="3">
+      Simple pricing for your business
+    </Text>
+      <Text
+        bg="blackAlpha.600"
+        bgClip='text'
+        fontSize='50px'
+        fontWeight='extrabold'
+        fontFamily="Serif"
+        left="10px"
+       >
+        Plans that are carefully created<br/> to suit your Business
+      </Text></Grid>
+      <SimpleGrid columns="3"m="5" row="3" bg="teal.600" w="80vh"shadow="5px 10px 10px" h="100vh" top="100vh" borderRadius="10px">
+        <Flex bg="white" w="10vh" m="2" h="10vh" borderRadius={"50px"}  shadow="5px 10px 10px"></Flex>
+        <Tabs variant='enclosed'>
+  <TabList outline="black">
+    <Tab  fontWeight="bold" shadow="5px 10px 10px"fontSize="40px"color="black"top="10px">Premium </Tab>
+    <Tab  fontWeight="bold" shadow="5px 10px 10px"fontSize="40px"color="black">Points</Tab>
+  </TabList>
+  <TabPanels>
+    <TabPanel >
+    <Box z-index="1"
+      top="80vh"
+      left="30px"
+      // border="2px solid black"
+      shadow="5px 5px 10px"
+      w="70vh"
+      h="55vh"
+    position="absolute" textAlign="center" color="black" fontSize="60px" m="5"  fontFamily="sans-serif">Premium Pro
+          <Heading fontSize="150px" fontFamily="initial">$329</Heading>
+          <Heading fontSize="38px">Billed just Once</Heading> <br />
+          <Button colorScheme='teal' color="black"fontSize="30px" variant='solid' w="40vh" h="9vh" shadow="5px 5px 5px ">Get Started</Button>
+        </Box>
+    </TabPanel>
+    <TabPanel>
+    
+    <Box m="8" fontSize="45px" z-index="1" position="absolute" fontWeight="bold"top="80vh"
+      left="60px" >Access this feature when you get this pricing packages for your business</Box><br/>
+        <UnorderedList  spacing={4} fontSize="40px" listStyleType="none" fontFamily="sans-serif" z-index="1" position="absolute" top="110vh" left="30px">
+  <ListItem><Icon viewBox='0 0 200 200' color='gray'>
   <path
     fill='currentColor'
     d='M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0'
-    position="absolute"
-    left="45.21%"
-    right= "53.96%"
-    top=" 52.64%"
-    bottom="46.27%"
-    background=" #FFFFFF"
+  />
+</Icon>   International calling and messaging API</ListItem>
+  <ListItem><Icon viewBox='0 0 200 200' color='gray'>
+  <path
+    fill='currentColor'
+    d='M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0'
   />
 </Icon>   Additional  Phone numbers</ListItem>
-  <ListItem  position="absolute"
-width="490px"
-height= "28px"
-left="688px"
-top="472px"
-fontFamily='Inter'
-fontStyle= "normal"
-fontWeight= "400"
-fontSize="18px"
-lineHeight=" 28px"
-color=" #2D3748"><Icon viewBox='0 0 200 200' color='teal'>
+  <ListItem><Icon viewBox='0 0 200 200' color='gray'>
   <path
     fill='currentColor'
     d='M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0'
-    position="absolute"
-    left="45.21%"
-    right= "53.96%"
-    top=" 57.93%"
-    bottom="40.99%"
-    background=" #FFFFFF"
   />
 </Icon>   Automatic messages via zaiper</ListItem>
-  <ListItem  position="absolute"
-width="490px"
-height= "28px"
-left="688px"
-top="516px"
-fontFamily='Inter'
-fontStyle= "normal"
-fontWeight= "400"
-fontSize="18px"
-lineHeight=" 28px"
-color=" #2D3748"><Icon viewBox='0 0 200 200' color='teal'>
+  <ListItem><Icon viewBox='0 0 200 200' color='gray'>
   <path
     fill='currentColor'
     d='M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0'
-    position="absolute"
-    left="45.21%"
-    right= "53.96%"
-    top=" 63.22%"
-    bottom="35.7%"
-    background=" #FFFFFF"
   />
 </Icon>   24/7 support and consulting</ListItem>
 </UnorderedList>
-  </GridItem>
-</Grid>
-  <GridItem colSpan={5} bg='RGBA(0, 0, 0, 0.09)' h="140vh" ></GridItem>
-</Grid> */}
+    </TabPanel>
+  </TabPanels>
+</Tabs>
+      </SimpleGrid>
+    
+    
   </>
   )
 }
